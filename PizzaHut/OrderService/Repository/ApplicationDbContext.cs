564f97099b5956e32ece41602649917287f63aaa
MarcosfCosta <marcus.a.costa@hotmail.com>
2020-04-29 23:28:48 +0100

    HealthChecks for OrderService

---
+++ PizzaHut/OrderService/Repository/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderService.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) :
+            base(options)
+        {
+        }
+
+        //public DbSet<Indicador> Indicadores { get; set; }
+        //public DbSet<BolsaValores> BolsasValores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Indicador>()
+            //    .HasKey(i => i.Sigla);
+            //modelBuilder.Entity<BolsaValores>()
+            //    .HasKey(b => b.Sigla);
+        }
+    }
+}
--- PizzaHut/OrderService/Startup.cs
+++ PizzaHut/OrderService/Startup.cs
@@ -1,12 +1,21 @@
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using OrderService.Repository;
 using System;
 using System.IO;
+using System.Linq;
+using System.Net.Mime;
 using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 
 namespace OrderService
 {
@@ -35,7 +44,30 @@ namespace OrderService
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
-            });           
+            });
+
+
+            //Health checks
+            //**NOTE** In order to healthchecks work it is necessary to add the following dependencies
+            // - AspNetCore.HealthChecks.SqlServer (in case sqlserver)
+            // - Microsoft.EntityFrameworkCore.SqlServer
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("SQLConnection")));
+
+            // Redis cache
+            //services.AddDistributedRedisCache(options =>
+            //{
+            //    options.Configuration =
+            //        Configuration.GetConnectionString("CacheRedis");
+            //    options.InstanceName = "Cache-APIIndicadores-";
+            //});
+
+            services.AddHealthChecks()
+                .AddSqlServer(Configuration.GetConnectionString("SQLConnection"), name: "SQL Connection");
+                //.AddRedis(Configuration.GetConnectionString("CacheRedis"), name: "cacheRedis");
+            services.AddHealthChecksUI();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,6 +99,38 @@ namespace OrderService
             {
                 endpoints.MapControllers();
             });
+
+            //Enable HealthChecks
+            app.UseHealthChecks("/status",
+               new HealthCheckOptions()
+               {
+                   ResponseWriter = async (context, report) =>
+                   {
+                       var result = JsonConvert.SerializeObject(
+                           new
+                           {
+                               statusApplication = report.Status.ToString(),
+                               healthChecks = report.Entries.Select(e => new
+                               {
+                                   check = e.Key,
+                                   ErrorMessage = e.Value.Exception?.Message,
+                                   status = Enum.GetName(typeof(HealthStatus), e.Value.Status)
+                               })
+                           });
+                       context.Response.ContentType = MediaTypeNames.Application.Json;
+                       await context.Response.WriteAsync(result);
+                   }
+               });
+
+            // Generate health checks endpoint
+            app.UseHealthChecks("/healthchecks-data-ui", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            // Ativa o dashboard para a visualização da situação de cada Health Check
+            app.UseHealthChecksUI();
         }
     }
 }
